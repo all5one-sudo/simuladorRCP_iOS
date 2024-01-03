711579fb8099f4399ee29c404b5834d8223759a5
Federico Villar <federico.villar@mi.unc.edu.ar>
2024-01-03 20:11:59 -0300

    Primer commit

--- simuladorRCP/ContentView.swift
+++ simuladorRCP/ContentView.swift
@@ -9,13 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            InicioView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Inicio")
+                }
+            EntrenamientoView()
+                .tabItem {
+                    Image(systemName: "sportscourt.fill")
+                    Text("Entrenamiento")
+                }
+            EvaluacionView()
+                .tabItem {
+                    Image(systemName: "doc.text.fill")
+                    Text("Evaluación")
+                }
+            AcercaDeView()
+                .tabItem {
+                    Image(systemName: "info.circle.fill")
+                    Text("Acerca de")
+                }
         }
-        .padding()
+    }
+}
+
+struct InicioView: View {
+    var body: some View {
+        Text("Esta es la página de inicio")
+    }
+}
+
+struct EntrenamientoView: View {
+    var body: some View {
+        Text("Esta es la página de entrenamiento")
+    }
+}
+
+struct EvaluacionView: View {
+    var body: some View {
+        Text("Esta es la página de evaluación")
+    }
+}
+
+struct AcercaDeView: View {
+    var body: some View {
+        Text("Esta es la página acerca de")
     }
 }
 
