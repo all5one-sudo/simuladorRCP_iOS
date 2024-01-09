a92f73bbe91b9aac7d763fa74c6f32f56020e860
Federico Villar <federico.villar@mi.unc.edu.ar>
2024-01-09 10:20:52 -0300

    Working app

--- simuladorRCP/ContentView.swift
+++ simuladorRCP/ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import CoreBluetooth
 import Charts
+import PDFKit
 
 struct ContentView: View {
     var body: some View {
@@ -20,11 +21,11 @@ struct ContentView: View {
                         Image(systemName: "doc.text.fill")
                         Text("Evaluación")
                     }
-                AcercaDeView()
+                /*AcercaDeView()
                     .tabItem {
                         Image(systemName: "info.circle.fill")
                         Text("Acerca de")
-                    }
+                    }*/
         }
     }
 }
@@ -88,7 +89,8 @@ struct InicioView: View {
 struct EntrenamientoView: View {
     @EnvironmentObject var btManager: BluetoothManager
     @State var timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
-    @State var lastPressureData: [PressureData] = [PressureData(time: Date(), value: 6.2)]
+    @State var lastPressureData: [PressureData] = [PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),
+    PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2),PressureData(time: Date(), value: 6.2)]
     @State var lastFrequencyData: [FrequencyData] = [FrequencyData(time: Date(), value: 120)]
     var body: some View {
         VStack {
@@ -98,31 +100,38 @@ struct EntrenamientoView: View {
                 .frame(maxWidth: .infinity, alignment: .center)
                 .multilineTextAlignment(.center)
                 .padding()
-            Chart {
-                ForEach(lastPressureData) { pressureData in
-                    LineMark(
-                        x: PlottableValue.value("Tiempo",pressureData.time),
-                        y: PlottableValue.value("Presion [cm]",pressureData.value)
-                    )    
-                    .foregroundStyle(Color.red)
-                    .interpolationMethod(.monotone)
+            GroupBox("Compresiones en el pecho") {
+                Chart {
+                    ForEach(lastPressureData) { pressureData in
+                        LineMark(
+                            x: PlottableValue.value("Tiempo",pressureData.time),
+                            y: PlottableValue.value("Presion [cm]",pressureData.value)
+                        )    
+                        .foregroundStyle(Color.red)
+                        .interpolationMethod(.monotone)
+                    }
+                    
                 }
-                
+                .chartYAxisLabel("Presión [cm]")
+                .chartXAxis(.hidden)
             }
-            .chartYAxisLabel("Presión [cm]")
             .padding()
-            Chart {
-                ForEach(lastFrequencyData) { frequencyData in
-                    LineMark(
-                        x: PlottableValue.value("Tiempo",frequencyData.time),
-                        y: PlottableValue.value("Frecuencia [bpm]",frequencyData.value)
-                    )    
-                    .foregroundStyle(Color.red)
-                    .interpolationMethod(.monotone)
+            GroupBox("Frecuencia de compresiones") {
+                Chart {
+                    ForEach(lastFrequencyData) { frequencyData in
+                        LineMark(
+                            x: PlottableValue.value("Tiempo",frequencyData.time),
+                            y: PlottableValue.value("Frecuencia [bpm]",frequencyData.value)
+                        )    
+                        .foregroundStyle(Color.red)
+                        .interpolationMethod(.monotone)
+                    }
+                    
                 }
-                
+                .chartYAxisLabel("Frecuencia [bpm]")
+                .chartXAxis(.hidden)
             }
-            .chartYAxisLabel("Frecuencia [bpm]")
+            .padding()
         }
         .onReceive(timer) { _ in
             self.lastPressureData = self.btManager.pressureData
@@ -132,8 +141,135 @@ struct EntrenamientoView: View {
     }
 }
 struct EvaluacionView: View {
+    @EnvironmentObject var btManager: BluetoothManager
+    @State private var isStarted = false
+    @State private var ended = false
+    @State private var lastPressure: PressureData? = nil
+    @State private var lastFrequency: FrequencyData? = nil
+    @State private var timer: Timer? = nil
+    @State private var insuf = 0
+    @State private var optim = 0
+    @State private var exces = 0
+    @State private var rightF = 0
+    @State private var wrongF = 0
+    @State private var pressureInfo: [PressureChart] = []
+    @State private var frequencyInfo: [FrequencyChart] = []
     var body: some View {
-        Text("Página de evaluación")
+        VStack {
+            Text("Modo de evaluación")
+                .font(.largeTitle)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .center)
+                .multilineTextAlignment(.center)
+                .padding()
+            if !ended {
+                Button(action: {
+                    //self.isStarted.toggle()
+                    //self.optim = 0
+                    //self.insuf = 0
+                    //self.exces = 0
+                    //self.wrongF = 0
+                    //self.rightF = 0
+                    self.lastPressure = self.btManager.pressureData.last
+                    self.lastFrequency = self.btManager.frequencyData.last
+                    self.isStarted.toggle()
+                    let pressData = self.btManager.pressureData.last
+                    let freqData = self.btManager.frequencyData.last
+                    if self.isStarted && !self.ended{
+                        self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+                            self.lastPressure = pressData
+                            self.lastFrequency = freqData
+                            let pressData = self.btManager.pressureData.last
+                            let freqData = self.btManager.frequencyData.last
+                            if let lastPressure = self.lastPressure, let pressData = pressData {
+                                if pressData.value <= 8.0 && pressData.value > 6.38 && pressData.value < lastPressure.value {
+                                    self.insuf = self.insuf + 1
+                                    print("Insuficiente: \(self.insuf)")
+                                }
+                                else if pressData.value > 5.42 && pressData.value <= 6.38 && pressData.value < lastPressure.value {
+                                    self.optim = self.optim + 1
+                                    print("Optima: \(self.optim)")
+                                }
+                                else if pressData.value <= 5.42 && pressData.value < lastPressure.value {
+                                    self.exces = self.exces + 1
+                                    print("Excesiva: \(self.exces)")
+                                }
+                            }
+                            if let freqData = freqData {
+                                if freqData.value > 100 && freqData.value <= 120 {
+                                    self.rightF = self.rightF + 1
+                                }
+                                else {
+                                    self.wrongF = self.wrongF + 1
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        self.ended = true
+                        self.isStarted = false
+                        print("optimas: \(self.optim)")
+                        print("insuficientes: \(self.insuf)")
+                        print("excesivas: \(self.exces)")
+                        self.pressureInfo.append(PressureChart(title: "Insuficientes", value: Double(self.insuf) / Double(self.insuf+self.exces+self.optim)))
+                        self.pressureInfo.append(PressureChart(title: "Óptimas", value: Double(self.optim) / Double(self.insuf+self.exces+self.optim)))
+                        self.pressureInfo.append(PressureChart(title: "Excesivas", value: Double(self.exces) / Double(self.insuf+self.exces+self.optim)))
+                        print(pressureInfo)
+                        self.frequencyInfo.append(FrequencyChart(title: "Óptimas", value: Double(self.rightF)/(Double(self.rightF)+Double(self.wrongF))))
+                        self.frequencyInfo.append(FrequencyChart(title: "Erroneas", value: Double(self.wrongF)/(Double(self.rightF)+Double(self.wrongF))))
+                        
+                    }
+                }) {
+                    Text(isStarted ? "Finalizar" : "Iniciar")
+                }
+            }
+            if ended {
+                HStack {
+                    GroupBox("Performance en compresiones") {
+                        Chart(pressureInfo) { pressure in 
+                            SectorMark(
+                                angle: .value(
+                                    Text(verbatim: pressure.title),
+                                    pressure.value
+                                )
+                            )    
+                            .foregroundStyle(by: .value("category", pressure.title))
+                        }
+                    }
+                    Spacer()
+                    GroupBox("Performance en frecuencia") {
+                        Chart(frequencyInfo) { frequency in 
+                            SectorMark(
+                                angle: .value(
+                                    Text(verbatim: frequency.title),
+                                    frequency.value
+                                )
+                            )    
+                            .foregroundStyle(by:
+                                    .value("category", frequency.title)
+                            )
+                        }
+                    }
+                }
+                if ended {
+                    Button(action: {
+                        self.ended = false
+                        self.isStarted = false
+                        self.optim = 0
+                        self.insuf = 0
+                        self.exces = 0
+                        self.wrongF = 0
+                        self.rightF = 0
+                        self.pressureInfo.removeAll()
+                        self.frequencyInfo.removeAll()
+                        self.isStarted = false
+                    }) {
+                        Text("Reiniciar")
+                    }
+                }
+            }
+        }
+        
     }
 }
 struct AcercaDeView: View {
@@ -141,7 +277,16 @@ struct AcercaDeView: View {
         Text("Acerca del proyecto")
     }
 }
-
+struct PressureChart: Identifiable {
+    let id = UUID()
+    let title: String
+    let value: Double
+}
+struct FrequencyChart: Identifiable {
+    let id = UUID()
+    let title: String
+    let value: Double
+}
 
 #Preview {
     ContentView()
